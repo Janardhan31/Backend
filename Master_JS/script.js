0ff5f5ffc51660041eb72648f88fb93de52f94cc
Janardhan Kumar <janardhankr2003@gmail.com>
2026-02-12 14:45:21 +0530

    JS

---
+++ Master_JS/script.js
@@ -0,0 +1,56 @@
+// fundamentals of JS
+// 1. arrays and objects
+// 2. funstions retunr
+// 3. async js coding
+// 4. foreach map filter find indexof
+
+
+// for each - used to do any task on each element of an array
+// var arr=[1,2,3,4,5]
+// arr.forEach(function(val){
+//     console.log(val + " Hello")
+// })
+
+
+// map - when we want to do any task on our given array and create a new array
+// var arr=[1,2,3,4,5]
+// var newarr = arr.map(function(val){
+//     return val*3
+// })
+
+// console.log(newarr)
+
+
+// filter - used to filter anyvalue of any condition
+// var arr=[1,2,3,4,5]
+// var ans = arr.filter(function(val){
+//     if(val>=3){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// })
+
+// console.log(ans)
+
+
+// find - always retutn the first value in array which is present
+// var arr=[1,2,3,4,5]
+// var ans = arr.find(function(val){
+//     if(val===2){
+//         return val;
+//     }
+// })
+
+// console.log(ans)
+
+
+
+// indexof - used to retunr the index of that element if present if not it'll return -1
+
+
+// Synchrnouse -  Jo code line by line chalega
+// ASynchrnouse - Asynchronous JavaScript allows tasks to run in the background without blocking the main thread, enabling other code to execute while waiting for operations like API calls or timers to complete
+
+
